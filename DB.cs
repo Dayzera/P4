037f730ff16d7ca7f6bbe95d6d5953fb4752153f
Dayzera <61506487+Dayzera@users.noreply.github.com>
2020-02-26 15:03:12 +0100

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace lab1
+{
+    public class DB
+    {
+        public void Select(System.Data.SqlClient.SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers";
+            var command = new SqlCommand(query, connection);
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+
+            reader.Close();
+          
+        }
+
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionID, regionDescription) VALUES (@id, '@description')";
+            var command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+
+
+        public void Delete(SqlConnection connection, string table, string description)
+        {
+            var query = "DELETE FROM @table WHERE @description = 'Bielsko'";
+            var command = new SqlCommand(query, connection);
+
+            command.Parameters.Add(table);
+            command.Parameters.Add(description);
+            var off = command.ExecuteNonQuery();
+            Console.WriteLine($"{off} row");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DB();
+            var connectionString = " ";
+            using var connection = new SqlConnection(connectionString);
+                    
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 12, "Bielsko");
+            db.Delete(connection, "Customers", "Bielsko");
+
+
+            connection.Close();
+        }
+    }
+}
