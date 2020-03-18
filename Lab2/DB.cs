3bb24c9bdab893b30398fce93cf32e88023febba
Dayzera <61506487+Dayzera@users.noreply.github.com>
2020-03-18 12:18:12 +0100

    Add files via upload

---
+++ Lab2/DB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Lab2P4
+{
+    class DB
+    {
+        public void Select(IDbConnection connection)
+        {
+            var regions = connection.Query<Region>("SELECT * FROM Region");
+
+            foreach (var item in regions)
+            {
+                Console.WriteLine($"{item.RegionID}: {item.RegionDescription}");
+            }
+                    
+        }
+
+        public void Insert(IDbConnection connection, Region region)
+        {
+            connection.Execute("INSERT INTO Region(RegionID, RegionDescription, VALUES(@is, @desc)", 
+                new
+                {
+                    id = id,
+                    desc = region.RegionDescription
+                });
+
+        }
+	public void Delete(string connection, int id)
+        {
+            using IDbConnection connect = new SqlConnection(connection);
+            
+            connect.Execute("DELETE FROM Region WHERE RegionID=@id", new { id = id, });
+
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+
+                var db = new DB();
+                db.Select(connection);
+                db.Insert(connection, 201, "d-test");              
+            }
+                                  
+        }
+    }
+}
